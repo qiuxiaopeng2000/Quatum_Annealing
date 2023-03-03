4fa9a4b675944c1ed8f10f28ac11c9da4b12de2e
xiaopengqiu <xiaopengqiu8@gmail.com>
2023-03-03 16:14:30 +0800

    Single-objective Optimization

--- nen/Result.py
+++ nen/Result.py
@@ -544,12 +544,14 @@ class ProblemResult:
         method1_objective = method1_result.get_wso_objective(w)
         method2_objective = method2_result.get_wso_objective(w)
         statistic, pvalue = stats.ranksums(np.array(method1_objective), np.array(method2_objective), alternative="less")
+        statistics = {method1: statistic, method2: statistic}
+        pvalues = {method1: pvalue, method2: pvalue}
         mean = {method1: np.mean(method1_objective), method2: np.mean(method2_objective)}
         std = {method1: np.std(method1_objective), method2: np.std(method2_objective)}
         max_num = {method1: np.max(method1_objective), method2: np.max(method2_objective)}
         min_num = {method1: np.min(method1_objective), method2: np.min(method2_objective)}
         elapsed = {method1: method1_result.method_result.elapsed, method2: method2_result.method_result.elapsed}
-        return statistic, pvalue, mean, std, max_num, min_num, elapsed
+        return statistics, pvalues, mean, std, max_num, min_num, elapsed
 
     def union_average_compare(self, union_method: str, average_method: str) -> List[Dict[str, float]]:
         """union_average_compare [summary] return union method compared with average method with scores indicated by
