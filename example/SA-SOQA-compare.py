1124c377dc16c1f6ec7e677c4668ae7e38d55e6b
xiaopengqiu <xiaopengqiu8@gmail.com>
2023-03-07 15:39:35 +0800

    Single-objective Optimization

--- example/SA-SOQA-compare.py
+++ example/SA-SOQA-compare.py
@@ -23,7 +23,7 @@ qp = QP(name, order)
 weights = {'cost': 1/2, 'revenue': 1/2}
 
 # solve with Genetic Algorithm
-result1 = FSAQPSolver.solve(problem=qp, weights=weights, t_max=100, t_min=0.0001, L=20,
+result1 = FSAQPSolver.solve(problem=qp, weights=weights, t_max=100, t_min=0.0001, L=10,
                             max_stay=50, sample_times=10, num_reads=1000)
 sa_result = MethodResult('sa', problem_result.path, qp)
 sa_result.add(result1)
