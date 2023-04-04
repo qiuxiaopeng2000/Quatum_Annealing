bae936ef826e620a1283bc55cf5c1e87a0ab62fc
xiaopengqiu <xiaopengqiu8@gmail.com>
2023-04-04 11:24:39 +0800

    experiment

--- experiment/RQ1/sa.py
+++ experiment/RQ1/sa.py
@@ -1,7 +1,8 @@
-# Load the project path
-# from project_path import PROJECT_PATH
-# import sys
-# sys.path.append(PROJECT_PATH)
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 
 from nen import QP, ProblemResult, MethodResult
 from nen.Solver import SAQPSolver
