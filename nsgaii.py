9b2f1662ee837e54562ef097919ca54f10e753eb
xiaopengqiu <xiaopengqiu8@gmail.com>
2023-04-05 16:43:11 +0800

    experiment

--- nsgaii.py
+++ nsgaii.py
@@ -25,7 +25,7 @@ for name in names_NRP:
     JarSolver.solve(
         solver_name='NSGAII', config_name='tmp_config',
         problem=name, objectiveOrder=order_NRP, iterations=6,
-        populationSize=1000, maxEvaluations=20000,
+        populationSize=1000, maxEvaluations=100000,
         crossoverProbability=0.8, mutationProbability=(1 / problem.variables_num),
         # resultFolder=result_folder, methodName='nsgaii', exec_time=-1
     )
@@ -49,7 +49,7 @@ for name in names_FSP:
     JarSolver.solve(
         solver_name='NSGAII', config_name='tmp_config',
         problem=name, objectiveOrder=order_FSP, iterations=6,
-        populationSize=1000, maxEvaluations=50000,
+        populationSize=1000, maxEvaluations=100000,
         crossoverProbability=0.8, mutationProbability=(1 / problem.variables_num),
         resultFolder=result_folder, methodName='nsgaii', exec_time=-1
     )
